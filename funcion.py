357aa484944240238130c553a78323eb4ba27784
Default user <oi.ochoa@uniandes.edu.co>
2019-10-22 18:36:30 +0000

    lo del cos, codigo e imagen

--- funcion.py
+++ funcion.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x=np.linspace(0,4*np.pi)
-y=np.sin(x)
+y=np.cos(x)
 plt.plot(x,y)
-plt.title("gráfica del sin")
+plt.title("gráfica del cos")
 plt.xlabel("x")
-plt.ylabel("sin(x)")
+plt.ylabel("cos(x)")
 plt.savefig("grafica.png")
